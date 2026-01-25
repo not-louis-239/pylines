9fdeee7743d2d415f950dba5b61f9282bf9cb4e4
not-louis-239 <louis.boulton@icloud.com>
2026-01-26 00:39:34 +0800

    Add collision checkers
    This is in preparation for adding building collision checks

---
+++ src/pylines/core/collision_checkers.py
@@ -0,0 +1,29 @@
+from pylines.core.custom_types import Coord3, RealNumber
+
+def point_in_cuboid(point: Coord3, cuboid_centre: Coord3, cuboid_dims: Coord3) -> bool:
+    x, y, z = point
+    cx, cy, cz = cuboid_centre
+    dx, dy, dz = cuboid_dims
+
+    return (cx - dx/2 <= x <= cx + dx/2 and
+            cy - dy/2 <= y <= cy + dy/2 and
+            cz - dz/2 <= z <= cz + dz/2)
+
+def point_in_cylinder(point: Coord3, cylinder_centre: Coord3, radius: RealNumber, height: RealNumber) -> bool:
+    # Assumes that +x = east, +z = south, +y = skyward
+
+    x, y, z = point
+    cx, cy, cz = cylinder_centre
+
+    # Check height along +y (skyward)
+    if not (cy - height/2 <= y <= cy + height/2):
+        return False
+
+    # Check radius in XZ plane
+    return (x - cx)**2 + (z - cz)**2 <= radius**2
+
+def point_in_sphere(point: Coord3, sphere_centre: Coord3, radius: RealNumber) -> bool:
+    x, y, z = point
+    cx, cy, cz = sphere_centre
+
+    return (x - cx)**2 + (y - cy)**2 + (z - cz)**2 <= radius**2
