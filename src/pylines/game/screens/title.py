deb34a4e940556f8b16b484594ff268a39925cd7
not-louis-239 <louis.boulton@icloud.com>
2026-02-07 12:17:16 +0800

    Add more controls text in title screen

--- src/pylines/game/screens/title.py
+++ src/pylines/game/screens/title.py
@@ -67,20 +67,44 @@ class TitleScreen(State):
 
         draw_text(self.display_surface, (C.WN_W//2, 0.95*C.WN_H), 'centre', 'centre', "Copyright (C) 2025-2026 Louis Masarei-Boulton.", (127, 127, 127), 15, self.fonts.monospaced)
 
-        draw_text(self.display_surface, (C.WN_W//2, C.WN_H*0.3), 'centre', 'centre', "Read Before Flight", (0, 192, 255), 40, self.fonts.monospaced)
+        draw_text(self.display_surface, (C.WN_W * 0.3, C.WN_H*0.3), 'centre', 'centre', "Read Before Flight", (0, 192, 255), 40, self.fonts.monospaced)
 
         controls: dict[str, str] = {
             "W/S": "Throttle",
-            "Arrows": "Pitch/Yaw",
             "Z/X": "Flaps Up/Down",
-            "B": "Brake",
             "A/D": "Rudder",
-            "Esc": "Pause",
+            "Arrows": "Pitch/Yaw",
+            "B": "Brake",
+            "G": "Cycle GPS dest.",
+            "Esc": "Pause"
         }
 
         for i, (key, desc) in enumerate(controls.items()):
-            draw_text(self.display_surface, (C.WN_W//2 - 140, C.WN_H * (0.41 + 0.05*i)), 'right', 'centre', key, WHITE, 27, self.fonts.monospaced)
-            draw_text(self.display_surface, (C.WN_W//2 + 80, C.WN_H * (0.41 + 0.05*i)), 'left', 'centre', desc, WHITE, 27, self.fonts.monospaced)
+            draw_text(self.display_surface, (C.WN_W//2 - 340, C.WN_H * (0.41 + 0.05*i)), 'right', 'centre', key, (150, 230, 255), 27, self.fonts.monospaced)
+            draw_text(self.display_surface, (C.WN_W//2 - 300, C.WN_H * (0.41 + 0.05*i)), 'left', 'centre', desc, WHITE, 27, self.fonts.monospaced)
+
+        draw_text(self.display_surface, (C.WN_W * 3/4, C.WN_H*0.3), 'centre', 'centre', "Map Controls", (0, 192, 255), 30, self.fonts.monospaced)
+
+        controls: dict[str, str] = {
+            "M": "Show/Hide Map",
+        }
+
+        for i, (key, desc) in enumerate(controls.items()):
+            draw_text(self.display_surface, (C.WN_W//2 + 230, C.WN_H * (0.38 + 0.05*i)), 'right', 'centre', key, (150, 230, 255), 27, self.fonts.monospaced)
+            draw_text(self.display_surface, (C.WN_W//2 + 270, C.WN_H * (0.38 + 0.05*i)), 'left', 'centre', desc, WHITE, 27, self.fonts.monospaced)
+
+        draw_text(self.display_surface, (C.WN_W * 3/4, C.WN_H*0.47), 'centre', 'centre', "While Map Open:", (0, 192, 255), 30, self.fonts.monospaced)
+
+        controls: dict[str, str] = {
+            "W/S": "Zoom In/Out",
+            "Arrows": "Pan",
+            "Space": "Re-centre",
+            "H (hold)": "Show advanced info",
+        }
+
+        for i, (key, desc) in enumerate(controls.items()):
+            draw_text(self.display_surface, (C.WN_W//2 + 230, C.WN_H * (0.55 + 0.05*i)), 'right', 'centre', key, (150, 230, 255), 27, self.fonts.monospaced)
+            draw_text(self.display_surface, (C.WN_W//2 + 270, C.WN_H * (0.55 + 0.05*i)), 'left', 'centre', desc, WHITE, 27, self.fonts.monospaced)
 
         self.settings_button.draw(self.display_surface)
         self.help_button.draw(self.display_surface)
